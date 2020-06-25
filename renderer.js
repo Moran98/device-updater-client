25f299fea1f814c94abac0d7d4d795e57986a69d
Aaron Moran <aaronmorann@protonmail.com>
2020-06-25 12:40:13 +0100

    updating

--- renderer.js
+++ renderer.js
@@ -7,3 +7,14 @@ var myObject = {
   }
   x = myObject.fullName();
   document.getElementById("demo").innerHTML = x; 
+
+  var myVar;
+
+            function myFunction() {
+            myVar = setTimeout(showPage, 3000);
+            }
+
+            function showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+            }
